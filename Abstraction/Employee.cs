19c2e63cf4bb2da5993834c30ac686293abc67d6
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-08 23:42:43 +0530

    Add project files.

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int id { get; set; }
+        public string email { get; set; }
+
+        public abstract void GetFullName();
+    }
+     class FullTimeEmployee: Employee
+    {
+        public override void GetFullName()
+        {
+            Console.WriteLine(id+" -- "+FirstName+" "+LastName+" -- "+email);
+        }
+    }
+}
---
+++ Abstraction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new FullTimeEmployee();
+
+            emp.FirstName = "Ayaz";
+            emp.LastName = "Ahmad";
+            emp.email = "Ayaj@gmail.com";
+            emp.id = 123;
+
+            emp.GetFullName();
+        }
+    }
+}
